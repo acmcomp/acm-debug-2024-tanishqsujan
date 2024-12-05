d38023bc847e693f9be916bcc10959c27ded0c96
tanishqsujan <tanishq.kothamasu@gmail.com>
2024-12-05 12:27:50 +0400

    Update Solution.js

--- Q4/Solution.js
+++ Q4/Solution.js
@@ -1,20 +1,19 @@
 function rotate(nums, k) {
-    k = k % nums.length;
-    reverse(nums, 0, nums.length);
-    reverse(nums, 0, k);
-    reverse(nums, k, nums.length);
+    k = k % nums.length; 
+    reverse(nums, 0, nums.length - 1); 
+    reverse(nums, 0, k - 1);           
+    reverse(nums, k, nums.length - 1); 
     return nums;
 }
-
 function reverse(nums, start, end) {
     while (start < end) {
         let temp = nums[start];
         nums[start] = nums[end];
         nums[end] = temp;
         start++;
-        end++;
+        end--; 
     }
 }
+console.log(rotate([1, 2, 3, 4, 5], 2)); 
+console.log(rotate([1, 2, 3], 4));       
 
-console.log(rotate([1, 2, 3, 4, 5], 2));
-console.log(rotate([1, 2, 3], 4)); 
